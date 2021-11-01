7c214a57dd82ede52bb5e9970bf20abb448c9202
MVCH812 <mvch812@gmail.com>
2021-11-01 18:08:19 +0300

    First

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿void FillArray(int[] A) // Метод, заполняющий массив случайными двузначными целыми числами от 10 до 100
+{
+    int length = A.Length;
+    int index = 0;
+    while (index < length)
+    {
+        A[index] = new Random().Next(10, 100);
+        Console.WriteLine(A[index]);
+        index++;
+
+    }
+}
+int[] A = new int[20];
+FillArray(A);
+int index = 0;
+while (index < a.Length)
+{
+
+    if (A[index] % 2 == 0 )
+
+}
